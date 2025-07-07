777bc7a63bd69c5f0e519145b5c79c4923f27a8c
kravetso <okravets.info@gmail.com>
2025-07-07 15:27:30 +0300

    class IndexModel added

---
+++ app/models/IndexModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\models;
+
+
+class IndexModel
+{
+    protected \mysqli $db;
+    public function __construct()
+    {
+        try{
+            $this->db = new \mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        }catch (\mysqli_sql_exception $e){
+            exit('DB connection error');
+        }
+    }
+
+    public function all(): array|bool
+    {
+        $statement = [];
+        $query = "SELECT name FROM photos";
+        $statement = $this->db->query($query);
+        if($statement){
+            return $statement->fetch_all(MYSQLI_ASSOC);
+        }
+        return false;
+    }
+
+    public function add($photo)
+    {
+        $query = "INSERT INTO photos (name) VALUES (?)";
+        $prepared = $this->db->prepare($query);
+        $prepared->bind_param('s', $photo);
+        return $prepared->execute();
+    }
+
+}
