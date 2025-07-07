9451f7c4e935da252b078331da8fb5bb5bcd996b
kravetso <okravets.info@gmail.com>
2025-07-07 15:25:45 +0300

    default file added

---
+++ app/views/templates/default.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/css/style.css">
+        <title><?= (isset($title) ? $title . ' | ' : '') . SITE_NAME ?></title>
+    </head>
+    <body>
+        <h1>Gallery</h1>
+        <?php include_once $this->getViewPath($viewName);?>
+        <footer>&copy2025</footer>
+        <script src="/js/script.js" defer></script>
+    </body>
+</html>
